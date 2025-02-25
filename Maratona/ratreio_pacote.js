02362ce0e799a323eb67149a4b283398400479ea
DEVR01001 <rafael.dev01001@gmail.com>
2025-02-25 11:15:22 -0400

    teste

--- Maratona/ratreio_pacote.js
+++ Maratona/ratreio_pacote.js
@@ -3,11 +3,11 @@
 
 // ================================================================================================
 
-let DivPergunta = document.getElementById("pergunta")
+let DivPergunta = document.getElementById("pergunta");
 
 let DivInput = document.getElementById("input_escolha")
 
-let DivResultado = document.getElementById("resultado")
+let DivResultado = document.getElementById("resultado");
 
 let btn_jogar = document.getElementById("btn_jogar");
 
@@ -19,90 +19,72 @@ let DivBtnInput = document.getElementById("btn_input");
 DivPergunta.innerHTML =`1 - Adicionar Pacote <br>
     2 - Atualizar Status do Pacote <br>
     3 - Rastrear Pacote <br>
-    4 - Sair <br>`
-
-
-
+    4 - Sair <br>`;
 
 
 btn_jogar.addEventListener("click", ()=>{
-    teste = DivInput.value
+    teste = DivInput.value.trim();
     // DivResultado.innerHTML=teste
 
-    Escolha(teste)
- 
+    Escolha(teste);
+
 
 })
 
 
 
+let pacotes= {};
+
 function Escolha(escolha){
-
-    let pacotes= {}
-
-    // DivResultado.innerHTML= escolha
-
-    if(escolha === '1'){
-        DivInputResult.innerHTML="<input id='input_teste' type='number' placeholder='Digite o número de rastreiamento:  '> "
-        DivBtnInput.innerHTML = "<button type='button'> Adicionar </button>"
-
-        DivBtnInput.addEventListener("click", ()=>{
-            let DiviEscolha = document.getElementById("input_teste").value;
-            numero_ratreio =  DiviEscolha;
-            result = pacotes[`${numero_ratreio}`] = "";
-            DivResultado.innerHTML="Pacote adicionado com sucesso"
-
-
-        })
-       
-
-
-    }else if(escolha === '2'){
-        DivInputResult.innerHTML="<input id='input_teste' type='number' placeholder='Digite o número de rastreiamento:  '> "
-        DivBtnInput.innerHTML = "<button type='button'> Adicionar </button>"
-        DivBtnInput.addEventListener("click", ()=>{
-            numero_ratreio = document.getElementById("input_teste").value;
-            for(let [key] of Object.keys(pacotes)){
-                if (numero_ratreio === key){
-                    novoStatus = DivInput.innerHTML = "<input id='input_teste' type='text' placeholder='Digite o novo Status:  '> "
-                    DivBtnInput.innerHTML = "<button type='button'> Adicionar </button>"
-                    .DivBtnInput.addEventListener("click", ()=>{
-                        
-                    })
-
-                    pacotes[`${numero_ratreio}`] = novoStatus
-                    result = "Status atualizado com sucesso."
-                    DivResultado.innerHTML= result
+    // let pacotes= {};
     
-                }else{
-                    result = "Pacote não exite."
-                   DivResultado.innerHTML= result
-    
-                }
+    let cont =0; 
+    let escolhas = escolha
+
+    switch(escolhas){
+        
+        case "1":
+            numero_ratreio = prompt("Digite o numero de rastreio: ");
+            result = pacotes[numero_ratreio] = "Status Padrão";
+            DivResultado.innerHTML="Pacote adicionado com sucesso";
+            break;
+        case "2":
+            numero_ratreio = prompt("Digite o numero de rastreio: ");
+            if (numero_ratreio in pacotes){
+                novostatus = prompt("Digite o novo status: ");
+                    pacotes[numero_ratreio] = novostatus;
+                    result = "Status atualizado com sucesso.";
+                    DivResultado.innerHTML= result;
+                    
+            }else{
+                result = "2 - Pacote não Econtrado.";
+                DivResultado.innerHTML= result;
+
+            }
+                
+            break;
+        case "3":
+            numero_ratreio = prompt("Digite o numero de rastreio: ");
+            if( numero_ratreio in pacotes){
+                DivResultado.innerHTML =`Status do pacote: ${pacotes[numero_ratreio]}`;
+
+            }else{
+                DivResultado.innerHTML="3 - Pacote não Econtrado ";
+
             }
 
-        })
-        
-    }else if (escolha === '3'){
-        numero_ratreio = DivInput.innerHTML = ` <input id="input_escolha" type="text" placeholder="Digite o número de rastreiamento:  "> `
-        statusPacote = pacotes[`${numero_ratreio}`]
-        result = `Status do Pacote: ${statusPacote}`
-       DivResultado.innerHTML= result
+            break;
+        case "4":
+            result = `Saindo do Programa.`;
+            DivResultado.innerHTML= result;
+            break;
 
-    }else if (escolha === '4'){
-        result = `Saindo do Programa.`
-        DivResultado.innerHTML= result
+
+
+        
 
     }
     
+           
     
-
-}
-
-
-
-
-
-
-
-DivResultado.innerHTML = pacotes
+}   
